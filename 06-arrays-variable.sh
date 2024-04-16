bcc2b5babfdb92757d85c4ea4481c69767d9de5b
vijaysai <vijaysai.chinthala45@gmail.com>
2024-04-16 22:04:16 +0530

    shell script practice

--- 06-arrays-variable.sh
+++ 06-arrays-variable.sh
@@ -3,4 +3,4 @@ CricketTeam = ("RCB" "SRH" "MI" "RR" "KKR" "DC" "CSK" "KINGX1" "LSG" "GT")
 
 echo "My fav Ipl team is ${CricketTeam[1]}"
 
-echo " All Ipl team are ${CricketTeam[@]}
+echo " All Ipl team are ${CricketTeam[@]}"

8d3b5e3dfc6276447d574e108e95522807b36075
vijaysai <vijaysai.chinthala45@gmail.com>
2024-04-16 20:41:50 +0530

    shell script practice

---
+++ .vs/scripting/variable.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+# above command is shebang here # means commeting expect the first line # will behave as the commeting for the code if you use below 
+
+echo "hi vijay , welcome to the shell scripting course"

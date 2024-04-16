ee8ec4985994a1d8f9308749fd319048faef563b
vijaysai <vijaysai.chinthala45@gmail.com>
2024-04-16 20:56:49 +0530

    shell script practice

---
+++ 02-variable.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+echo "vijay:hi uday kiran , how are you man"
+echo "uday:i am good vijay, how about you"
+echo "vijay:i am also good where are you staying"
+echo "uday:currently in hyderabad, can we meet in weekend"
+echo "vijay:sure uday bye"
+
+

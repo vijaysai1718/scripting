bbaa440bd8e7cca44cd36d54fb747f015a736692
vijaysai <vijaysai.chinthala45@gmail.com>
2024-04-16 22:11:51 +0530

    shell script practice

---
+++ 07-dataType-variable.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+echo "enter the number 1 value to be added"
+read  Number1
+
+echo "enter the number 2 value to be added"
+read  Number2
+
+Sum=$((Number1+Number2))
+
+echo "sum of the 2 values is : ${Sum}"

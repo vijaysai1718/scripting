1960479c691dcc23e20afc29f1e055148dd5bc7c
vijaysai <vijaysai.chinthala45@gmail.com>
2024-04-16 21:10:37 +0530

    shell script practice

---
+++ 04-declaring-variable-runtime.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+#decalring the variable 
+
+Name1=$1
+Name2=$2
+
+echo $Name1:"hi $Name2, How are you"
+echo $Name2:"hi $Name1, Fine yar what about you"
+echo $Name1:"Good"
+echo $Name2:"okay vijay will text you after sometime"

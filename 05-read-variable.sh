0dc901f5984378d7f13884b647f80743da1ddddc
vijaysai <vijaysai.chinthala45@gmail.com>
2024-04-16 21:31:36 +0530

    shell script practice

---
+++ 05-read-variable.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+echo "please enter the username :"
+
+read -s Username
+
+echo "please enter the password:"
+
+read -s Password
+
+echo "user as entred username as $Username and password as $Password"

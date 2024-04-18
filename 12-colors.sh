ba37bf29df87e6a68227d6123145687ee29067b3
vijaysai <vijaysai.chinthala45@gmail.com>
2024-04-18 23:35:55 +0530

    new changes to the scripting files

--- 12-colors.sh
+++ 12-colors.sh
@@ -5,7 +5,7 @@ Timestamp=$(date +%H-%M-%S)
 scriptName=$(echo $0 | cut -d "." -f1)
 LogFile=/tmp/$scriptName-$Timestamp.log
 
-Red="\e[31m"
+Red="\e[31m"  #red color 
 Green="\e[32m"
 Normal="\e[0m"
 
@@ -18,16 +18,22 @@ echo "user is a super user he can install the software what he want"
 fi
 
 validate()
-if [ $1 -ne 0 ]
+if [ $1 -ne 0 ]  #$1 means first arugment  $2 is the second arugment 
 then 
+# we have to enable the color so we are appening with -e and after that we are giving normal color to everything for that reason normal is append at the last 
 echo -e "$2 is  $Red failed $Normal please check the logs"
 else
 echo -e "$2 is $Green success $Normal"
 fi
 
-dnf install github -y &>>$LogFile
+# &>> means it will store  the both error logs and success logs into the file 
+# 1>> means it will only  append the success logs to the file
+# 2>> means it will only  append the error logs to the file
+dnf install github -y &>>$LogFile 
 
-validate $? "Installation of the github"
+# $? this means it will give the pervious command status if 0 means it was success other wise failure 
+
+validate $? "Installation of the github" # vaiidate is the function name we are calling the function with the 2 arugments.
 
 dnf install docker -y &>>$LogFile
 

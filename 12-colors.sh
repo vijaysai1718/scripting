e36de99c0c6ef1a6bb6bbcca232da7e0d3bcb67e
vijaysai <vijaysai.chinthala45@gmail.com>
2024-04-18 23:24:44 +0530

    new changes to the scripting files

---
+++ 12-colors.sh
@@ -0,0 +1,34 @@
+#!/bin/bash
+
+userid=$(id -u)
+Timestamp=$(date +%H-%M-%S)
+scriptName=$(echo $0 | cut -d "." -f1)
+LogFile=/tmp/$scriptName-$Timestamp.log
+
+Red="/e[31m"
+Green="/e[32m"
+Normal="/e[0m"
+
+if [ $userid -ne 0 ]
+then 
+echo "user is not root user he cant install the software"
+exit 1
+else
+echo "user is a super user he can install the software what he want"
+fi
+
+validate()
+if [ $1 -ne 0 ]
+then 
+echo -e "$2 is  $Red failed $Normal please check the logs"
+else
+echo -e "$2 is $Green success $Normal"
+fi
+
+dnf install github -y 1>>$LogFile
+
+validate $? "Installation of the github"
+
+dnf install docker -y 1>>$LogFile
+
+validate $? "Installation of the docker"

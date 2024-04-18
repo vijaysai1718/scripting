f069864f6204f1c869996bf84581f406615e73e5
vijaysai <vijaysai.chinthala45@gmail.com>
2024-04-18 22:16:23 +0530

    installation changes

--- 10-installation.sh
+++ 10-installation.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
-user = $(id -u)
+user=$(id -u)
 
-if [ $user -ne 0]
+if [ $user -ne 0 ]
 then 
 echo "Please get the super user access to perform this installation on software"
 exit 1
@@ -10,21 +10,20 @@ echo "you are having the super acess to install the software"
 
 dnf install mysql -y
 
-if [ $? -ne 0]
+if [ $? -ne 0 ]
 then
 echo "installation failed due to some issues, please check the logs"
 exit1
 else
-echo "Mysql installation was successful "
+echo "Mysql installation was successful"
 fi
 
 dnf install github -y
 
-if [ $? -ne 0]
+if [ $? -ne 0 ]
 then
 echo "installation failed due to some issues, please check the logs"
 exit1
 else
-echo "github installation was successful "
+echo "github installation was successful"
 fi
-

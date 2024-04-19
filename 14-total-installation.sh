789329a2e4147184764e670e0db5b26f8889a966
vijaysai <vijaysai.chinthala45@gmail.com>
2024-04-19 13:16:35 +0530

    new changes to the scripting files

---
+++ 14-total-installation.sh
@@ -0,0 +1,38 @@
+user=$(id -u)
+scriptName=$(echo $0 | cut -d "." -f1)
+timestamp=$(date + %H-%M-%S)
+logFile=/tmp/$scriptName-$timestamp.log
+
+red="\e[32m"
+green="\e[33m"
+yellow="\e[34m"
+
+normal="\e[0m"
+
+if [ user -ne 0 ]
+then 
+echo "He is not having the access to run the script, Please get the root access"
+else
+echo "User is a root user"
+fi
+
+validate()
+if[ $1 -ne 0 ]
+then
+echo -e "Installation of the $2 is  $red failure  $normal please check the logs for more details"
+else
+echo "Installation of the $2 is $green success"
+fi
+
+for i in $@
+do
+ echo "package to install: $i"
+    dnf list installed $i &>>$logFile
+if[ $? -eq 0]
+then 
+echo "$1 already installed in the server. $yellow skipping it"
+else
+dnf install $i -y &>>$logFile
+validate $? $i
+
+done
